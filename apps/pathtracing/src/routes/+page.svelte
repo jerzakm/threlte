f7c4aefa069bcb98573accbcc3220495fc0e5775
jerzakm <jerzakm@gmail.com>
2023-02-21 09:23:52 +0100

    pathtracign setup

---
+++ apps/pathtracing/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import App from './App.svelte';
+</script>
+
+<div class="w-screen h-screen fixed">
+	<App />
+</div>
---
+++ apps/pathtracing/src/routes/Scene.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { OrbitControls, T } from '@threlte/core';
+	import { Grid } from '@threlte/extras';
+	import { DoubleSide } from 'three';
+</script>
+
+<Grid sectionThickness={0} />
+
+<T.PerspectiveCamera makeDefault position={[5, 5, 5]}>
+	<OrbitControls enabled />
+</T.PerspectiveCamera>
+
+<!-- <T.Mesh material={tMat}>
+	<T.BoxGeometry args={[3, 3, 3]} />
+</T.Mesh> -->
+
+<T.Mesh position.x={3}>
+	<T.BoxGeometry />
+	<T.MeshStandardMaterial color={'red'} side={DoubleSide} />
+</T.Mesh>
+
+<T.DirectionalLight intensity={2} />
+<T.AmbientLight intensity={2} />
+
+<T.Mesh>
+	<T.SphereGeometry args={[100]} />
+	<T.MeshBasicMaterial color={'#666666'} side={DoubleSide} />
+</T.Mesh>
