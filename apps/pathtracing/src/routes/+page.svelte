9be973164cd80c219360244b272a161813306a33
jerzakm <jerzakm@gmail.com>
2023-02-21 21:59:30 +0100

    implementing..

--- apps/pathtracing/src/routes/+page.svelte
+++ apps/pathtracing/src/routes/+page.svelte
@@ -1,7 +1,15 @@
 <script>
+	import { onMount } from 'svelte';
 	import App from './App.svelte';
+
+	let mounted = false;
+	onMount(() => {
+		mounted = true;
+	});
 </script>
 
-<div class="w-screen h-screen fixed">
-	<App />
+<div class="w-screen h-screen fixed" id="container">
+	{#if mounted}
+		<App />
+	{/if}
 </div>
